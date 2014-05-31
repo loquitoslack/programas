4a603a48216237d756699b3dd3f6d18abb61743a
loquitoslack <loquitoslack@gmail.com>
2014-05-31 11:45:19 -0500

    corregido
    Se corrige y se comprueba algunos comandos se ordena la sintaxis de html

--- scripts/sarg_generation_daily_weekly_montly.sh
+++ scripts/sarg_generation_daily_weekly_montly.sh
@@ -14,56 +14,61 @@
 #   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 #
 # RELEASE: 20140530
+# Autor: exonix 
 # Autor: Edwin Enrique Flores Bautista
 # email: loquitoslack@gmail.com 
 # blog: loquitoslack.blogspot.com 
-# Fecha: 30-05-12
-#VER=20050202
-+ ''
-+ . /etc/sarg/sarg-reports.conf
-++ SARG=/usr/bin/sarg
-++ CONFIG=/etc/sarg/sarg.conf
-++ HTMLOUT=/var/lib/sarg
-+++ hostname
-++ PAGETITLE='Access Reports on proxy'
-++ LOGOIMG=/sarg/images/sarg.png
-+++ hostname
-++ LOGOLINK=http://proxy/
-++ DAILY=Daily
-++ WEEKLY=Weekly
-++ MONTHLY=Monthly
-++ EXCLUDELOG1='SARG: No records found'
-++ EXCLUDELOG2='SARG: End'
-++ /bin/mktemp
-+ TMPFILE=/tmp/tmp.ws6bA0PJp4
-+ ERRORS=/tmp/tmp.ws6bA0PJp4.errors
-+ MANUALDATE=
-+ case "$(uname)" in
-++ uname
-++ date -date today +%d/%m/%Y
-+ TODAY=07/12/2011
-++ date -date '1 day ago' +%d/%m/%Y
-+ YESTERDAY=06/12/2011
-++ date -date '1 week ago' +%d/%m/%Y
-+ WEEKAGO=30/11/2011
-++ date -date '1 month ago' +01/%m/%Y
-++ date -date '1 month ago' +31/%m/%Y
-+ MONTHAGO=01/11/2011-31/11/2011
-+ export LC_ALL=C
-+ LC_ALL=C
-+ case $1 in
-+ weekly
-+ WEEKLYOUT=/var/lib/sarg/Weekly
-+ mkdir -p /var/lib/sarg/Weekly
-+ create_index_html
-+ echo -e '  <html>\n  <head>\n  <title>Access Reports on proxy</title>\n  </head>\n  <body>\n  <div align=center>\n    <a href=http://proxy/><img border=0 src=/sarg/images/sarg.png></a>\n    <table border=0 cellspacing=6 cellpadding=7>\n      <tr>\n        <th align=center nowrap><b><font face=Arial size=4 color=green>Access Reports on proxy</font></b></th>\n      </tr>\n      <tr>\n        <td align=center bgcolor=beige><font face=Arial size=3><a href=Daily>Daily</a></font></td>\n      </tr>\n      <tr>\n        <td align=center bgcolor=beige><font face=Arial size=3><a href=Weekly>Weekly</a></font></td>\n      </tr>\n      <tr>\n        <td align=center bgcolor=beige><font face=Arial size=3><a href=Monthly>Monthly</a></font></td>\n      </tr>\n    </table>\n  </div>\n  </body>\n  </html>'
-+ /usr/bin/sarg -f /etc/sarg/sarg.conf -d week-1 -o /var/lib/sarg/Weekly
-+ exclude_from_log
-+ grep -v 'SARG: End'
-+ grep -v 'SARG: No records found'
-+ cat /tmp/tmp.ws6bA0PJp4.errors
-+ rm -f /tmp/tmp.ws6bA0PJp4 /tmp/tmp.ws6bA0PJp4.errors
-in "sarg-report" i have:
+# Fecha: 31-05-12
+# VER=20140531
+
+######### Define Variables ################
+
+SARG=/usr/bin/sarg
+CONFIG=/etc/sarg/sarg.conf
+HTMLOUT=/var/www/squid-reports 
+PAGETITLE='Access Reports on proxy'
+LOGOIMG=/sarg/images/sarg.png
+LOGOLINK=http://4.bp.blogspot.com/-Y1GEIePzPOQ/T_yjwuhfw8I/AAAAAAAAAAg/bIRt8AuoFrc/s1600/LOGO+OFICIAL+UCH.png
+DAILY=Daily
+WEEKLY=Weekly
+MONTHLY=Monthly
+EXCLUDELOG1='SARG: No records found'
+EXCLUDELOG2='SARG: End'
+TMPFILE=/tmp/tmp.ws6bA0PJp4
+ERRORS=/tmp/tmp.ws6bA0PJp4.errors
+TODAY=`date --date today +%d/%m/%Y`
+YESTERDAY=`date --date '1 day ago' +%d/%m/%Y`
+WEEKAGO=`date --date '1 week ago' +%d/%m/%Y`
+WEEKAGO01=`date --date '1 month ago' +01/%m/%Y`
+WEEKAGO31=`date --date '1 month ago' +31/%m/%Y`
+MONTHAGO=echo "$WEEKAGO01-$WEEKAGO31"
+WEEKLYOUT=/var/lib/sarg/Weekly
+mkdir -p /var/lib/sarg/Weekly
+create_index_html(){
+echo -e '
+<html>\n  
+<head>\n  
+<title>Access Reports on proxy</title>\n
+</head>\n  
+<body>\n  
+<div align=center>\n    
+<a href=http://proxy/><img border=0 src=/sarg/images/sarg.png></a>\n 
+<table border=0 cellspacing=6 cellpadding=7>\n 
+    <tr>\n        
+      <th align=center nowrap>
+        <b><font face=Arial size=4 color=green>Access Reports on proxy</font></b>
+      </th>\n      
+    </tr>\n      
+    <tr>\n        
+      <td align=center bgcolor=beige><font face=Arial size=3><a href=Daily>Daily</a></font></td>\n      </tr>\n      <tr>\n        <td align=center bgcolor=beige><font face=Arial size=3><a href=Weekly>Weekly</a></font></td>\n      </tr>\n      <tr>\n        <td align=center bgcolor=beige><font face=Arial size=3><a href=Monthly>Monthly</a></font></td>\n      </tr>\n    </table>\n  </div>\n  </body>\n  </html>'
+/usr/bin/sarg -f /etc/sarg/sarg.conf -d week-1 -o /var/lib/sarg/Weekly
+exclude_from_log
+grep -v 'SARG: End'
+grep -v 'SARG: No records found'
+cat /tmp/tmp.ws6bA0PJp4.errors
+rm -f /tmp/tmp.ws6bA0PJp4 /tmp/tmp.ws6bA0PJp4.errors
+} 
+
 today ()
 {
   DAILYOUT=$HTMLOUT/$DAILY
